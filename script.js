1b2a4927e677e9e1d55bd27bc1ad54cd480ae1d5
moye <moyerock@gmail.com>
2025-12-20 11:32:38 +0900

    feat: simple resume on my github.io

--- script.js
+++
@@ -1,3 +0,0 @@
-function pad(n){return n.toString().padStart(2,'0')}
-function update(){const d=new Date();const t=`${pad(d.getHours())}:${pad(d.getMinutes())}:${pad(d.getSeconds())}`;document.getElementById('time').textContent=t;document.getElementById('date').textContent=d.toDateString()}
-setInterval(update,1000);update();
